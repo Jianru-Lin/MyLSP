32aff1010f10ebf732fe2fceade89a907d8caa30
Jianru Lin <jianru.lin@gmail.com>
2015-04-22 13:17:24 +0800

    更多针对 CriticalSection 的单元测试

--- UnitTest-Lightup/CriticalSection.cpp
+++ UnitTest-Lightup/CriticalSection.cpp
@@ -6,3 +6,20 @@ TEST(CriticalSectionTest, EnterLeave) {
 	cs.Enter();
 	cs.Leave();
 }
+
+TEST(CriticalSectionTest, NewDelete) {
+	CriticalSection *cs = new CriticalSection();
+	delete cs;
+}
+
+TEST(CriticalSectionTest, EnterDelete) {
+	CriticalSection *cs = new CriticalSection();
+	cs->Enter();
+	delete cs;
+}
+
+TEST(CriticalSection, LeaveDelete) {
+	CriticalSection *cs = new CriticalSection();
+	cs->Leave();
+	delete cs;
+}
