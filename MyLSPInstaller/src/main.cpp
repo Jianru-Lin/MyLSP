279e0ece38557142320a583767f2c948e31f8f5b
Jianru Lin <jianru.lin@gmail.com>
2015-04-22 15:07:09 +0800

    MyLSPInstaller 可以枚举协议链了

--- MyLSPInstaller/src/main.cpp
+++ MyLSPInstaller/src/main.cpp
@@ -1,6 +1,21 @@
 #include <Lightup.h>
+#include <stdio.h>
 
 int main()
 {
+	EasyWSCEnumProtocols enumProtocols;
+	if (!enumProtocols.Invoke())
+	{
+		return 1;
+	}
+
+	printf("count: %d\n", enumProtocols.ProtocolListLength());
+	
+	for (int i = 0, len = enumProtocols.ProtocolListLength(); i < len; ++i)
+	{
+		LPWSAPROTOCOL_INFOW protocol = &enumProtocols.ProtocolList()[i];
+		wprintf(L"[%d] %ws\n", protocol->dwCatalogEntryId, protocol->szProtocol);
+	}
+
 	return 0;
 }
